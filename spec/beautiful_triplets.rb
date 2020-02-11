d4b7dd489fceb6593619b2e4413462b22280007f
Ahmed Magdy <ahmadmgdi94@gmail.com>
2020-02-11 21:05:17 +0200

    Format Specs

--- spec/beautiful_triplets.rb
+++ spec/beautiful_triplets.rb
@@ -5,9 +5,9 @@ RSpec.describe BeautifulTriplets, '()' do
     @test = BeautifulTriplets.new
   end
   describe '#testcase' do
-    it 'return 3' do 
+    it 'return 3' do
       result = @test.count_triplets(3, [1, 2, 4, 5, 7, 8, 10])
-      expect(result).to eq(3)  
+      expect(result).to eq(3)
     end
   end
 end
